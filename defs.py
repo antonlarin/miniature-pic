18f71fede6a3f144d6b4beac65a929f6393c6006
Anton Larin <edding.default@gmail.com>
2016-08-14 20:28:15 +0300

    Replace run setup with plane wave

--- defs.py
+++ defs.py
@@ -1,48 +1,42 @@
 import math
 
 # common constants
-COARSE_GRID_SIZE = 1024
-PML_SIZE = 12
 C =  2.99792458e+10
 PI = 3.14159265358979
 EM = 0.910938215e-27
 E = -4.80320427e-10
 
-wavelength = 0.8e-4
-W0 = 2*PI*C/wavelength
-REL_FIELD = -2*PI*EM*C*C/(E*wavelength)
-dx = wavelength / 32
-dt = dx/(4*C)
-x0 = -COARSE_GRID_SIZE * dx / 2
+# run setup
+resolution_multiplier = 1
+courant_factor = 3
+COARSE_GRID_SIZE = 384 * resolution_multiplier
+dx = 6.0 / COARSE_GRID_SIZE
+wavelength = 8 * dx * resolution_multiplier
+wavenumber = 2 * PI / wavelength
+angular_frequency = 2 * PI * C / wavelength
+periods = 4
+pulse_duration = periods * wavelength / C
+pulse_delay = 4 * pulse_duration
+dt = dx / (C * courant_factor)
+x0 = 0
 
-FINE_GRID_SIZE = 256 # in coarse grid cell units
+PML_SIZE = 12
+
+FINE_GRID_SIZE = 130 * resolution_multiplier # in coarse grid cell units
 DEBUG_PADDING = 2
 AUX_GRID_SIZE = 1
 
-ITERATIONS = 5000
-OUTPUT_PERIOD = 200
+ITERATIONS = courant_factor * resolution_multiplier * 600
+OUTPUT_PERIOD = courant_factor * resolution_multiplier * 12
 
 
 # pulse description
-def sgn(x):
-    return 1 if x > 0 else (-1 if x < 0 else 0)
-
-def block(x, xmin, xmax):
-    return (sgn(x - xmin) + sgn(xmax - x)) * 0.5
-
-def f(t):
-    return math.sin(W0*t/20)
-
-def form(t):
-    return f(t)*f(t)*f(t*20)*block(t, 0, 20*PI/W0)
-
-DELAY = 16 * wavelength / C
-
-def shape(r, t):
-    return 2 * REL_FIELD * form(t + r/C - DELAY)
+def pulse_longitudinal_profile(t):
+    return math.exp(-((t - pulse_delay)/pulse_duration)**2 * 2 * math.log(2))
 
 def pulse(x, t):
-    return shape(abs(x), t)
+    return (math.sin(wavenumber * x - angular_frequency * t) *
+            pulse_longitudinal_profile(t))
 
 
 # source fields
@@ -53,8 +47,8 @@ def left_e(x, t):
     return pulse(x, t)
 
 def right_b(x, t):
-    return -pulse(x, t)
+    return 0
 
 def right_e(x, t):
-    return pulse(x, t)
+    return 0
 
